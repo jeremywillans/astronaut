dc74d00bb6778a9fbf33368dee87e606a9aed81a
Jeremy Willans <jeremy@willans.id.au>
2021-04-29 15:25:54 +1000

    Code Refactor

--- app.js
+++ app.js
@@ -16,43 +16,42 @@ try {
   }
   config = {
     token: process.env.TOKEN,
-    ownerId: process.env.OWNER_ID,
+    removeDeviceRegistrationsOnStart: true,
+    messageFormat: 'markdown',
   };
   if (process.env.WEBHOOK_URL) {
     config.webhookUrl = process.env.WEBHOOK_URL;
     config.port = process.env.PORT || 3000;
-    config.webhookSecret = process.env.SECRET || 'replacemwithasecretstring';
+    // eslint-disable-next-line operator-linebreak
+    config.webhookSecret =
+      process.env.SECRET || 'replace-me-with-a-secret-string';
   }
 } catch (error) {
   debug(`Error: ${error}`);
 }
 
-// init express
-const app = express();
-app.use(express.json());
+let app;
+// Init Express, if configured
+if (config.webhookUrl) {
+  app = express();
+  app.use(express.json());
+}
 
-// init framework
+// Init Framework
 const framework = new Framework(config);
 framework.start();
 
-// An initialized event means your webhooks are all registered and the
-// framework has created a bot object for all the spaces your bot is in
+// Framework Initialized
 framework.on('initialized', () => {
-  framework.debug('Framework initialized successfully! [Press CTRL-C to quit]');
+  debug('Framework initialized successfully! [Press CTRL-C to quit]');
 });
 
-// A spawn event is generated when the framework finds a space with your bot in it
-// You can use the bot object to send messages to that space
-// The id field is the id of the framework
-// If addedBy is set, it means that a user has added your bot to a new space
-// Otherwise, this bot was in the space before this server instance started
+// Handle Spawn Event
 framework.on('spawn', (bot, id, addedBy) => {
   if (!addedBy) {
-    // don't say anything here or your bot's spaces will get
+    // don't say anything here or your bots spaces will get
     // spammed every time your server is restarted
-    framework.debug(
-      `Framework created an object for an existing bot in a space called: ${bot.room.title}`,
-    );
+    debug(`Execute spawn in existing space called: ${bot.room.title}`);
     if (bot.room.type === 'group') {
       debug(`Execute processing for existing space: ${bot.room.title}`);
       bot.say(`RoomId: ${bot.room.id}\nBye!`).then(() => bot.exit());
@@ -64,10 +63,7 @@ framework.on('spawn', (bot, id, addedBy) => {
       debug(`Execute spawn processing for space: ${bot.room.title}`);
       bot.framework.webex.people.get(addedBy).then((personObject) => {
         bot.say(`RoomId: ${bot.room.id}\nBye!`).then(() => bot.exit());
-        bot.dm(
-          config.ownerId,
-          `Space Identification used by ${personObject.emails[0]}`,
-        );
+        debug(`Space Identification used by ${personObject.emails[0]}`);
       });
     }
   }
@@ -79,31 +75,32 @@ framework.hears(/.*/gim, (bot, trigger) => {
   if (bot.room.type === 'group') {
     bot.framework.webex.people.get(trigger.person.id).then((personObject) => {
       bot.say(`RoomId: ${bot.room.id}\nBye!`).then(() => bot.exit());
-      bot.dm(
-        config.ownerId,
-        `Space Identification used by ${personObject.emails[0]}`,
-      );
+      debug(`Space Identification used by ${personObject.emails[0]}`);
     });
   } else {
-    bot.say('Hello %s!', trigger.person.displayName);
-    bot.framework.webex.people.get(trigger.person.id).then((personObject) => {
-      bot.dm(config.ownerId, `Bot Hello used by ${personObject.emails[0]}`);
-    });
+    bot.say(`Hello ${trigger.person.displayName}!`);
+    debug(`Bot Hello used by ${trigger.person.emails[0]}`);
   }
 });
 
-// define express path for incoming webhooks
-app.post('/framework', webhook(framework));
+let server;
+// Init Server, if configured
+if (config.webhookUrl) {
+  // Define Express Path for Incoming Webhooks
+  app.post('/framework', webhook(framework));
 
-// start express server
-const server = app.listen(config.port, () => {
-  framework.debug('Framework listening on port %s', config.port);
-});
+  // Start Express Server
+  server = app.listen(config.port, () => {
+    framework.debug('Framework listening on port %s', config.port);
+  });
+}
 
-// gracefully shutdown (ctrl-c)
+// Gracefully Shutdown (CTRL+C)
 process.on('SIGINT', () => {
-  framework.debug('stoppping...');
-  server.close();
+  debug('Stopping...');
+  if (config.webhookUrl) {
+    server.close();
+  }
   framework.stop().then(() => {
     process.exit();
   });
